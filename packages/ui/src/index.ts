7efa50cb8c1f316b947180926356a519222654e4
soushians noorghorbani <snoorghorbani@gmail.com>
2026-02-07 21:03:13 +0300

    chore: cleanup unused components

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,7 +1,5 @@
 export * from "./ImageGrid";
 export * from "./ProfileGrid";
 export * from "./button";
-export * from "./card";
-export * from "./code";
 export * from "./logo";
 export * from "./header";
